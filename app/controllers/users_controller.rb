15ec5121cf7d91bb964598aecc9e9aee73ce7e8f
dlim09 <danielhlim1990@gmail.com>
2016-10-24 16:18:38 -0700

    added update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,18 @@ class UsersController < ApplicationController
 	before_action :current_user, only:[:edit, :update, :destroy]
 	before_action :require_correct_user, only: [:show, :edit, :update, :destroy]
 	
+
+	def create
+	end
+
+	def update_page
+	end
+
+	def update_user
+		user_update = User.update(first_name:params[:first_name_update], last_name:params[:last_name_update], email:params[:email_update])
+		address_update = Address.update(street:params[:street_update], city:params[:city_update], state:params[:state_update], zip:params[:zip_update])
+
+		# redirect to the main user page
+		redirect_to '' 
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root "pages#index"
 
+  get '/update' => 'users#update_page'
 
-
+  post '/users/update/:id' => 'users#update_user'
 end
