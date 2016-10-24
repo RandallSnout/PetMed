1e154e1f661482280d0322213861c98dccb225a1
dlim09 <danielhlim1990@gmail.com>
2016-10-24 14:21:37 -0700

    added routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,5 @@
 class UsersController < ApplicationController
+	before_action :current_user, only:[:edit, :update, :destroy]
+	before_action :require_correct_user, only: [:show, :edit, :update, :destroy]
+	
 end
