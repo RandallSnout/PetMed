c65cd1cb6a6531fbdfb62088a93b480989c3a355
“Randall <pfeiffer.justin.pfeiffer@gmail.com>
2016-10-24 14:51:32 -0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
 
   root "pages#index"
-  get 'users#index'
-  get 'users#new'
-  post 'users#create'
-  get 'users#show'
-  get 'users#edit'
-  patch 'users#update'
-  delete 'users#destroy'
+
 
 
 end
