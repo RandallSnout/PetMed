b6aae3d7fc3f2c46b9ff01e3f5665f2beb575f3d
dlim09 <danielhlim1990@gmail.com>
2016-10-24 20:46:02 -0700

    added pets update page

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,12 @@
 class PetsController < ApplicationController
+	def pet_update_page
+		@pet = Pet.find(params[:id])
+		@pin = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flattenstring = (0...50).map { o[rand(o.length)] }.join
+	end
+
+	def update
+		pet = Pet.find(params[:id]).update(name:params[:pet_name_update], sex:params[:pet_name_update], species:params[:pet_species_update], age:params[:pet_age_update], color:params[:pet_color_update])
+		# redirects to the user's page
+		redirect_to ''
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,16 @@ Rails.application.routes.draw do
   root "pages#index"
 
 # ------- [get] ----------------------------
-
   get '/update/user' => 'users#update_user_page'
 
   get '/update/vet' => 'users#update_vet_page'
+
+  get '/update/pet' => 'pet#update_pet_page'
 
 # ------- [patch] ----------------------------
   patch '/users/update/:id' => 'users#update_user'
 
   patch '/vet/update/:id' => 'users#update_vet'
+
+  patch '/pet/update/:id' => 'pet#update'
 end
