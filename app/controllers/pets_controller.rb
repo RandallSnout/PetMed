dcdf616410f6299f60a482e8d1622bd7bc11e99e
dlim09 <danielhlim1990@gmail.com>
2016-10-25 18:37:50 -0700

    new routes

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,4 +1,9 @@
 class PetsController < ApplicationController
+
+	def pet_main
+	end
+	
+
 	def pet_update_page
 		@pet = Pet.find(params[:id])
 	end
@@ -19,7 +24,7 @@ class PetsController < ApplicationController
 	end
 
 	def update
-		pet = Pet.find(params[:id]).update(name:params[:pet_name_update], sex:params[:pet_sex_update], species:params[:pet_species_update], age:params[:pet_age_update], color:params[:pet_color_update])
+		@pet = Pet.find(params[:id]).update(name:params[:pet_name_update], sex:params[:pet_sex_update], species:params[:pet_species_update], age:params[:pet_age_update], color:params[:pet_color_update])
 		# redirects to the user's page
 		redirect_to "/users/profile/#{current_user.id}"
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root "pages#index"
   get "pages/finder" => "pages#finder"
   get "pages/about" => "pages#about"
-
   get "sessions/new" => "sessions#new"
   get "user/profile" => "users#users_profile"
   get "users/:id" => "users#users_profile"
@@ -11,9 +10,8 @@ Rails.application.routes.draw do
 
 # ------- [post] ----------------------------
   post 'sessions/signin' => 'sessions#login'
-	post "users/register" => "users#create_user"
-	post "vets/register" => "users#create_vet"
-
+  post "users/register" => "users#create_user"
+  post "vets/register" => "users#create_vet"
   post "pets/create" => "pets#create"
 
 # ------- [get] ----------------------------
@@ -34,7 +32,7 @@ Rails.application.routes.draw do
 
   patch '/pet/update/:id' => 'pet#update'
 
-  # ------- [destroy] ----------------------------
+# ------- [destroy] ----------------------------
 	delete 'sessions/logout' => 'sessions#destroy'
 
 end
