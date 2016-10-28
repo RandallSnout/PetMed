6e040b60ec62e21fe2403577fe655fc1877bda52
“Randall <pfeiffer.justin.pfeiffer@gmail.com>
2016-10-27 21:58:17 -0700

    Style Updates and pet profile page creation.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,8 +2,10 @@ class PetsController < ApplicationController
 
 	def profile
 		@current = Pet.find(params[:id])
+		@shots = Shot.where("pet_id = #{params[:id]}").all
+		@record = Record.where("pet_id = #{params[:id]}").first
+		@comments = Vet.joins(:comments).select("note", "first_name", "last_name", "comments.created_at", "pet_id").where("pet_id = #{params[:id]}")
 	end
-	
 
 	def pet_update_page
 		@pet = Pet.find(params[:id])
@@ -16,6 +18,7 @@ class PetsController < ApplicationController
 		pin = [*('a'..'z'),*('0'..'9')].shuffle[0,9].join
 		pet = Pet.new(name:params[:pet_name], sex:params[:pet_sex], species:params[:pet_species], age:params[:pet_age], color: params[:pet_color], breed:params[:pet_breed], pin:pin, user_id: current_user.id, avatar:params[:avatar])
 		if pet.save
+			Record.create(pet_id: pet.id)
 			redirect_to "/users/#{current_user.id}"
   		else
   			flash[:errors] = pet.errors.full_messages
