485c78addba42ae2f156e7c6fa5423346a33708b
“Randall <pfeiffer.justin.pfeiffer@gmail.com>
2016-12-12 17:39:43 -0800

    revert

--- db/migrate/20161027183709_create_comments.rb
+++ db/migrate/20161027183709_create_comments.rb
@@ -2,12 +2,12 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :note
-      t.references :pet, index: true
-      t.references :vet, index: true
+      t.references :pets, index: true
+      t.references :vets, index: true
 
       t.timestamps null: false
     end
-    add_foreign_key :comments, :pet
-    add_foreign_key :comments, :vet
+    add_foreign_key :comments, :pets
+    add_foreign_key :comments, :vets
   end
 end
