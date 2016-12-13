75861acca5d763b4184726219ab90766da39b243
Justin Pfeiffer <pfeiffer.justin.pfeiffer@gmail.com>
2016-12-12 18:09:10 -0800

    Update 20161028024258_change_all_associations_to_singular.rb

--- db/migrate/20161028024258_change_all_associations_to_singular.rb
+++ db/migrate/20161028024258_change_all_associations_to_singular.rb
@@ -1,8 +1,8 @@
 class ChangeAllAssociationsToSingular < ActiveRecord::Migration
   def change
 
-  	 rename_column :shots, :pets_id, :pet_id
-  	 rename_column :records, :pets_id, :pet_id
+  	# rename_column :shots, :pets_id, :pet_id
+  	# rename_column :records, :pets_id, :pet_id
 
 
   end
