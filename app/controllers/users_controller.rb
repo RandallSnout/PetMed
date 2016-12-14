cfd15c5beacfb5d0e58e9c03d44ed3b3e08e2595
“Randall <pfeiffer.justin.pfeiffer@gmail.com>
2016-12-13 17:35:04 -0800

    qwe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 	def users_profile 
 		@user = User.joins(:address).select("first_name", "last_name","phone_number", "street", "state", "city", "zip", "avatar_file_name").find(current_user.id)
 		@pets = Pet.where("user_id = #{current_user.id}")
-		# @a = Vet.joins(:users, :address).select("vets.id as VETS_ID", "vets.first_name as VETS_FN","vets.last_name as VETS_LN", "vets.email", "users.id as USER_ID", "users.first_name as USERS_FN", "users.last_name as USERS_LN", :street, :city, :state, :city).find_by("users.id == #{current_user.id}")
+		@a = Vet.select("vets.id as VETS_ID", "vets.first_name as VETS_FN","vets.last_name as VETS_LN").find_by("users.id == #{current_user.id}")
 	end
 
 	def create_user
