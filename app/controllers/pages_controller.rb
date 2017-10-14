974ae6ced5110f221028cd8250f470bc3e3b434b
“Randall <pfeiffer.justin.pfeiffer@gmail.com>
2017-10-13 21:35:12 -0700

    remove users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,7 @@ class PagesController < ApplicationController
 			@a = Vet.joins(:users, :address).select("vets.id as VETS_ID", "vets.first_name as VETS_FN","vets.last_name as VETS_LN", "vets.email", "users.id as USER_ID", "users.first_name as USERS_FN", "users.last_name as USERS_LN", :street, :city, :state, :city).find_by("users.id = #{current_user.id}")
 		end
 
-		@users = User.select(:first_name, :last_name, :email)
+		# @users = User.select(:first_name, :last_name, :email)
 	end
 
 	def about
